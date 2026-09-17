eb6e659ee814322c8688973243d128cd8075c2d1
Prashant Kumar <prashantbaghel98@gmail.com>
2026-09-17 15:10:05 +0530

    mongodb config update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+let isConnected = false;
 
-const connectDB = async ()=>{
-    mongoose.connect(process.env.MONGO_URI)
-    console.log("DB Connected")
-}
-module.exports = connectDB
+const connectDB = async () => {
+    if (isConnected) {
+        return;
+    }
+
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is not defined");
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        isConnected = true;
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error.message);
+        throw error;
+    }
+};
+
+module.exports = connectDB;
