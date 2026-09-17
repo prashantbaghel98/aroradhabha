b22b02212167d9c033e110fb0e5b1ab6814ed835
Prashant Kumar <prashantbaghel98@gmail.com>
2026-09-17 14:29:18 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ connectDB()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173","https://arora-frontend.vercel.app/"],
+    origin:["http://localhost:5173","https://arora-frontend.vercel.app"],
     credentials:true
 }))
 
