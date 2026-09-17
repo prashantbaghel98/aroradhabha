c1ae9f1fa7955e81aab972a9c86d652af1d56bb4
Prashant Kumar <prashantbaghel98@gmail.com>
2026-09-17 14:55:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const env = require('dotenv/config')
+require("dotenv/config");
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 const cors = require('cors')
@@ -54,9 +54,9 @@ app.use("/api/food", foodRoute);
 
 // API Test
 
-// app.get("/", (req, res) => {
-//     res.send("API Working");
-// });
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
 
 
