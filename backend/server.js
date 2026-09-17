2805cbb90e711c9ca9513e36cd93fde0f3a43cf4
Prashant Kumar <prashantbaghel98@gmail.com>
2026-09-17 13:48:24 +0530

    PORT env

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ app.get("/", (req, res) => {
 // Local development server
 
 if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT, () => {
         console.log(
-            `Server is running on port ${PORT}`
+            `Server is running on port ${process.env.PORT}`
         );
     });
 }
